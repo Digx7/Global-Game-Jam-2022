7f024fc1fb1a7c2adf3c369baa5a9dba8314ef91
Digx7 <37136301+Digx7@users.noreply.github.com>
2022-01-28 19:15:37 -0800

    Got Dodge action working
    The dodge action now makes players not take damage from a bullet
    
    Next need focus on the following
    - end lag for actions (so players can take multiple actions in a round)
    - Countdown and breathing room btwn rounds

--- Assets/Scripts/Dodge_Action.cs
+++ Assets/Scripts/Dodge_Action.cs
@@ -5,6 +5,8 @@ using UnityEngine;
 public class Dodge_Action : MonoBehaviour
 {
 
+    [SerializeField] private Bullet_Detector Bullet_Detector;
+
     public void Dodge(Action input){
       if(input == Action.dodge) Dodge();
     }
@@ -13,5 +15,7 @@ public class Dodge_Action : MonoBehaviour
       Debug.Log("Dodge Action");
       //needs to play dodge animation
       //needs to turn off health breifly
+
+      Bullet_Detector.Set_CanTakeDamage(false); // needs to turn this back on after a bit
     }
 }
